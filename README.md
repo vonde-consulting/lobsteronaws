6640196b6cc8d0cbc13a21144af745296563ffc2
Ruihong Huang <ruihong.huang@gmail.com>
2020-05-14 17:45:56 +0100

    Update Readme

---
+++ README.md
@@ -0,0 +1,100 @@
+# LOBSTER on AWS 
+ The beta version of LOBSTER engine developed by [frischedaten UG](https://lobsterdata.com) has been designed to 
+ utilize the powerful computation and storage capacity of Amazon AWS. It runs on top of Apache Spark,
+ which are standard packages in [Amazon AWS EMR](https://aws.amazon.com/emr/) release. 
+ In this project, I develop a python wrapper that
+ 1. Sets up and starts an EMR cluster;
+ 2. Runs the order book construction as a step of the EMR; 
+ 3. Shuts down the cluster for saving costs once task is completed; and finally
+ 4. Reviews the order book. 
+ 
+ ## Prerequire
+ In order to try out the new powerful LOBSTER engine, you need
+ 1. An Amazon AWS account and an IAM user who is allowed creating and running EMR;
+ 2. An [S3 bucket](https://docs.aws.amazon.com/AmazonS3/latest/gsg/CreatingABucket.html) for storing the output; 
+ 3. [Amazon AWS CLI Version 1 installation](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html) 
+    on your local machine 
+ 2. Python 3 installation on you local machine. For people who like to use Spark to read demo output, the python version
+    should be ``3.7``, because ``pyspark`` does not support ``3.8`` so far; and
+ 4. Python packages: ``pandas`` and ``pyarrow``. Additional ``pyspark`` for people like to use Spark.
+ 
+ Note that this version of wrapper uses the default role setting of EMR, please make sure that those roles exist and are 
+ visible for the user.
+ ![EMR role](images/emr_default_roles.png)
+ A simple way to create all those roles is create a cluster in EMR with Quick Options in 
+ [EMR interface in Amazon S3 console](https://eu-west-2.console.aws.amazon.com/elasticmapreduce/).
+ 
+ ## Steps to Run the Demo Construction
+ After cloning the repository, please familiarise yourself of the usage of ``construct_order_book.py``.
+ ```bash
+$ python construct_order_book.py --help
+usage: construct_order_book.py [-h] -t TASK_FILE -k KEY_NAME -i INPUT_PATH -o
+                               OUTPUT_PATH [-g INSTANCE_GROUPS] [-j JAR_FILE]
+                               [-f OUTPUT_FORMAT] [-r REGION]
+                               [-p CREDENTIAL_PROFILE] [-n NUM_PARTITIONS]
+
+Construct order book using LOBSTER engine
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t TASK_FILE, --task_file TASK_FILE
+                        S3 task file. Example file: s3://demo-ordermessage-
+                        lobsterdata-com/NASDAQ100-2019-12-30.txt
+  -k KEY_NAME, --key_name KEY_NAME
+                        EMR pair key name
+  -i INPUT_PATH, --input_path INPUT_PATH
+                        Input path. The default is s3://demo-ordermessage-
+                        lobsterdata-com
+  -o OUTPUT_PATH, --output_path OUTPUT_PATH
+                        Output path. Required. S3 location.
+  -g INSTANCE_GROUPS, --instance_groups INSTANCE_GROUPS
+                        Instance group configure JSON file
+  -j JAR_FILE, --jar_file JAR_FILE
+                        The jar file contains LOBSTER engine. The default
+                        location is s3://bookconstructor-lobsterdata-com/com-
+                        lobsterdata-bookconstructor_2.11-0.1.jar
+  -f OUTPUT_FORMAT, --output_format OUTPUT_FORMAT
+                        Output format. It could be parquet or csv. The default
+                        is parquet
+  -r REGION, --region REGION
+                        EMR region. Default is eu-west-2
+  -p CREDENTIAL_PROFILE, --credential_profile CREDENTIAL_PROFILE
+                        AWS credential profile. Default is 'default'
+  -n NUM_PARTITIONS, --num_partitions NUM_PARTITIONS
+                        The number of partitions for output files. Lobster
+                        engine always partitions the output by symbols. If
+                        choose 0, the number of the partition will be
+                        automatically determined by Spark.
+ ``` 
+ * TASK_FILE: Required argument. The file contains the all requests, including stock, start date, end date and order book level. 
+    an example of request file has been stored at s3://demo-ordermessage-lobsterdata-com/NASDAQ100-2019-12-30.txt.
+    This file contains 103 requests of 10-level order book on 30th December, 2019.
+ * KEY_NAME: Required argument. It is the 
+    [Amazon EC2 key pair](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html) 
+    for you to access the EMR cluster. 
+ * INPUT_PATH: Required argument. S3 prefix for original ITCH message data.
+    <!-- 
+    * LOBSTER official subscriber: the LOBSTER team is preparing a quarter data for beta test. 
+      The location and the IAM guidance will be release to you soon.
+      -->
+    * Demo: the LOBSTER team has 30th December 2019 data open to public at  s3://demo-ordermessage-lobsterdata-com
+ * OUTPUT_PATH: Required argument. S3 prefix for output the data. Note that the two more sub-prefix will create for the output. 
+    * OUTPUT_PATH/log: the log information of the construction
+    * OUTPUT_PATH/orderbook: the output order book data partitioned by date
+ * INSTANCE_GROUPS: Optional argument. It is the uniformed instance configuration for the cluster. 
+    If not provided, the wrapper will pick up ``demo_config/construct_book_instance_groups.json`` as the configure file.
+    This configuration include auto-scaling setting. 
+    It could one to ten m5.4xlarge EC2 instances, depending the loading.
+ * JAR_FILE: Optional argument. This is the Jar file contains LOBSTER engine. The current location is
+    s3://bookconstructor-lobsterdata-com/com-lobsterdata-bookconstructor_2.11-0.1.jar.
+ * OUTPUT_FORMAT: Optional argument. It could be parquet or csv. The default is parquet.
+ * REGION: Optional argument. The region of cluster. The default is eu-west-2, i.e. London.
+ * CREDENTIAL_PROFILE: Optional argument. The user's 
+    [credential file](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html) 
+    for creating EMR cluster. Default is default. 
+ * NUM_PARTITIONS: Optional argument. It is the number of partitions of the output order book. If it is set to zero,
+    the number of the partitions will be determined by Spark configuration. 
+    Note that in any case, LOBSTER will partition the output by ``date`` and ``symbol``. 
+    So the number of partitions could be simply understood as the number of output files in a day.
+ 
+        
---
+++ construct_order_book.py
@@ -0,0 +1,197 @@
+import tempfile
+
+from aws.emr import LobsterEMR
+
+
+def prepare_instance_group_json_file():
+    _file_name = tempfile.NamedTemporaryFile().name + ".json"
+    with open(_file_name, 'w') as f:
+        f.write("""
+[
+  {
+    "InstanceCount": 1,
+    "BidPrice": "10.000",
+    "EbsConfiguration": {
+      "EbsBlockDeviceConfigs": [
+        {
+          "VolumeSpecification": {
+            "SizeInGB": 32,
+            "VolumeType": "gp2"
+          },
+          "VolumesPerInstance": 2
+        }
+      ]
+    },
+    "InstanceGroupType": "MASTER",
+    "InstanceType": "c4.xlarge",
+    "Name": "Master - 1"
+  },
+  {
+    "InstanceCount": 2,
+    "BidPrice": "10.000",
+    "EbsConfiguration": {
+      "EbsBlockDeviceConfigs": [
+        {
+          "VolumeSpecification": {
+            "SizeInGB": 64,
+            "VolumeType": "gp2"
+          },
+          "VolumesPerInstance": 1
+        }
+      ]
+    },
+    "InstanceGroupType": "CORE",
+    "InstanceType": "m5.4xlarge",
+    "Name": "Core-BookConstruct-Demo",
+    "AutoScalingPolicy":
+    {
+     "Constraints":
+      {
+       "MinCapacity": 1,
+       "MaxCapacity": 10
+      },
+     "Rules":
+     [
+      {
+       "Name": "Default-scale-out",
+       "Description": "Replicates the default scale-out rule in the console for YARN memory.",
+       "Action":{
+        "SimpleScalingPolicyConfiguration":{
+          "AdjustmentType": "CHANGE_IN_CAPACITY",
+          "ScalingAdjustment": 1,
+          "CoolDown": 300
+        }
+       },
+       "Trigger":{
+        "CloudWatchAlarmDefinition":{
+          "ComparisonOperator": "LESS_THAN",
+          "EvaluationPeriods": 1,
+          "MetricName": "YARNMemoryAvailablePercentage",
+          "Namespace": "AWS/ElasticMapReduce",
+          "Period": 300,
+          "Threshold": 10,
+          "Statistic": "AVERAGE",
+          "Unit": "PERCENT",
+          "Dimensions":[
+             {
+               "Key" : "JobFlowId",
+               "Value" : "${emr.clusterId}"
+             }
+          ]
+        }
+       }
+      }
+     ]
+   }
+  }
+]
+        """)
+        return _file_name
+
+
+def prepare_steps_json_file(s3_request_file, s3_input_path, s3_output_path,
+                            s3_jar="s3://bookconstructor-lobsterdata-com/com-lobsterdata-bookconstructor_2.11-0.1.jar",
+                            output_format="parquet", num_partitions=0):
+    _file_name = tempfile.NamedTemporaryFile().name + ".json"
+    with open(_file_name, mode='w') as f:
+        f.write(f"""
+    [
+        {{
+            "Args": [
+                "spark-submit",
+                "--deploy-mode",
+                "client",
+                "--driver-memory",
+                "6G",
+                "--driver-cores",
+                "3",
+                "--executor-memory",
+                "11G",
+                "--executor-cores",
+                "3",
+                "--class",
+                "com.lobsterdata.app.ConstructBook",
+                "{s3_jar}",
+                "{s3_request_file}",
+                "{s3_input_path}",
+                "{s3_output_path}",
+                "{output_format}",
+                "{num_partitions}"
+            ],
+            "Type": "CUSTOM_JAR",
+            "ActionOnFailure": "TERMINATE_CLUSTER",
+            "Jar": "command-runner.jar",
+            "Properties": "",
+            "Name": "Lobster Book Construction"
+        }}
+    ]
+        """)
+
+    return _file_name
+
+
+def main():
+    import argparse
+    import os
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    parser = argparse.ArgumentParser(description="Construct order book using LOBSTER engine")
+    parser.add_argument('-t', '--task_file', required=True, help="""S3 task file. 
+                            Example file: s3://demo-ordermessage-lobsterdata-com/NASDAQ100-2019-12-30.txt""")
+    parser.add_argument('-k', '--key_name', required=True, help="EMR pair key name")
+    parser.add_argument('-i', '--input_path', required=True,
+                        help="Input path. The default is s3://demo-ordermessage-lobsterdata-com")
+    parser.add_argument('-o', '--output_path', required=True, help="Output path. Required. S3 location.")
+    parser.add_argument('-g', '--instance_groups',
+                        default=os.path.join(dir_path, "demo_config", "construct_book_instance_groups.json"),
+                        help="Instance group configure JSON file")
+    parser.add_argument('-j', '--jar_file',
+                        default="s3://bookconstructor-lobsterdata-com/com-lobsterdata-bookconstructor_2.11-0.1.jar",
+                        help="The jar file contains LOBSTER engine. The default location is " +
+                             "s3://bookconstructor-lobsterdata-com/com-lobsterdata-bookconstructor_2.11-0.1.jar")
+    parser.add_argument('-f', '--output_format', default='parquet',
+                        help="Output format. It could be parquet or csv. The default is parquet")
+    parser.add_argument('-r', '--region', default="eu-west-2", help="EMR region. Default is eu-west-2")
+    parser.add_argument('-p', '--credential_profile', default="default",
+                        help="AWS credential profile. Default is 'default'")
+    parser.add_argument('-n', '--num_partitions', type=int,
+                        default=10,
+                        help="""The number of partitions for output files. 
+                        Lobster engine always partitions the output by symbols. If choose 0, the number of the 
+                        partition will be automatically determined by Spark. """)
+
+    args = parser.parse_args()
+
+    task_file = args.task_file
+    instance_groups = args.instance_groups
+    input_path = args.input_path
+    output_path = args.output_path
+    output_format = args.output_format
+    num_partitions = args.num_partitions
+    jar_file = args.jar_file
+    key_name = args.key_name
+    region = args.region
+    profile = args.credential_profile
+    log_uri = output_path + "/log"
+    confirmation = input(f"""
+    You are about to start the following task on AWS:
+    * Task file: {task_file}
+    * Instance groups: {instance_groups}
+    * Input path: {input_path}
+    * Output path: {output_path}
+    * Output format: {output_format}
+    * Number of partition: {num_partitions} (0 means using Spark default number)
+    * Lobster engine jar file: {jar_file}
+    * EMR pair key: {key_name}
+    * Region: {region}
+    * Log location: {log_uri}
+    Please confirm (yes or no) >> """)
+    if confirmation == 'yes':
+        step_file = prepare_steps_json_file(task_file, input_path, output_path, jar_file,
+                                            output_format, num_partitions)
+        construction_emr = LobsterEMR(instance_groups, step_file, key_name, name='Lobster Book Construction',
+                                      region=region, credential_profile=profile, log_uri=log_uri)
+        construction_emr.create()
+
+
+if __name__ == "__main__":
+    main()
