692655447377e4aa8b9cab2cb865da930e004d7a
Ruihong Huang <ruihong.huang@gmail.com>
2020-05-18 15:53:21 +0100

    0.2 release

---
+++ README.txt
@@ -0,0 +1,4 @@
+# LOBSTER on AWS
+ The beta version of LOBSTER engine developed by [frischedaten UG](https://lobsterdata.com) has been designed to
+ utilize the powerful computation and storage capacity of Amazon AWS. It runs on top of Apache Spark,
+ which are standard packages in [Amazon AWS EMR](https://aws.amazon.com/emr/) release.
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='lobsteronaws',
+    version='0.2',
+    author="Ruihong Huang",
+    author_email="huang@lobsterdata.com",
+    packages=find_packages(),
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vonde-consulting/lobsteronaws",
+)
