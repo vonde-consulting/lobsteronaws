b7ca1aa855cf068df0903c23095545f14181e07e
Ruihong Huang <ruihong.huang@gmail.com>
2020-05-18 15:57:33 +0100

    version to 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.txt", "r") as fh:
 
 setup(
     name='lobsteronaws',
-    version='0.1',
+    version='0.1.2',
     author="Ruihong Huang",
     author_email="huang@lobsterdata.com",
     packages=find_packages(),
